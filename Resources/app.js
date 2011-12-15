dea6853586cb9d4bc41e2ef608dd459d4240614c
Sharry Stowell <sharry@molinto.com>
2011-12-15 12:20:57 +0000

    Adding login/cookie when adding a place

--- Resources/app.js
+++ Resources/app.js
@@ -5,29 +5,41 @@
 var credentials = require('credentials').C;
 var cocoafish = require('cocoafish_module');
 
-//create Cocoafish Client
 var client = new cocoafish.Client(credentials.COCOAFISH_APP_NAME, credentials.COCOAFISH_API_KEY);
 
-// LOGIN INTO THE APP
+// login to the app
 client.login({
 	'login' : "sharry@molinto.com",
 	'password' : "password"
 }, function(e) {
 	if(e.success === true) {
-		Ti.API.info("login " + JSON.stringify(e));
-		alert(JSON.stringify(e));
+		Ti.API.info("logged in ok: " + JSON.stringify(e));
 	} else {
 		Ti.API.error(e.error);
 	}
 });
 
-// SEARCH FOR A PLACE
-client.place(function(e) {
+
+
+client.searchPlaces(function(e) {
 	if(e.success === true) {
-		//Ti.API.info("place " + JSON.stringify(e));
-		//alert(JSON.stringify(e));
 		alert("Status: "+e.response.meta.status +"\n" + e.response.meta.total_results + " place(s) found!");
 	} else {
 		Ti.API.error(e.error);
 	}
 });
+
+
+/** CREATE A PLACE - SESSION MUST EXISTS FIRST! **/
+client.createPlace({
+	'name' : "My house",
+	'latitude': "37.782227",
+    'longitude': "-122.393159"
+}, function(e) {
+	if(e.success === true) {
+		alert("Status: "+e.response.meta.status +"\n Place created!");
+	} else {
+		Ti.API.error(e.error);
+	}
+});
+
--- Resources/cocoafish_module.js
+++ Resources/cocoafish_module.js
@@ -1,18 +1,12 @@
 function Client(applicationName, applicationkey) {
 	this.COCOAFISH_APP_NAME = applicationName;
 	this.COCOAFISH_APPLICATION_KEY = applicationkey;
-	
 	this.ENDPOINT = 'https://api.cocoafish.com/v1/';
-	
 	this.CURRENT_USER = {};
-	this.CURRENT_PLACE = {};
-
 	//Ti.API.debug(JSON.stringify(this));
 }
 
-/*
- LOGIN FUNCTION
- */
+/*  LOGIN FUNCTION  */
 Client.prototype.login = function(args, callback) {
 	var that = this;
 	var xhr = Ti.Network.createHTTPClient();
@@ -44,20 +38,19 @@ Client.prototype.login = function(args, callback) {
 			"response" : xhr.responseText
 		});
 		
-		// SAVE THE CURRENT USER FOR LATER
-		var respObject = JSON.parse(xhr.responseText);
-		that.CURRENT_USER = respObject.response.users[0];
-		Ti.API.info("Saved current user " + that.CURRENT_USER);
+		var respObject= JSON.parse(xhr.responseText);
+		Ti.App.Properties.setString('session_id', respObject.meta.session_id);
+		//that.CURRENT_USER = respObject.response.users[0];
+		//Ti.API.info("Saved current user " + that.CURRENT_USER);
 	};
 
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
 	xhr.open('POST', that.ENDPOINT+"users/login.json?key=" + that.COCOAFISH_APPLICATION_KEY);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send(params);
 };
 
 /*
- PLACE FUNCTION
+ SEARCH PLACE FUNCTION
 */
 Client.prototype.searchPlaces = function(callback) {
 	var that = this;
@@ -95,9 +88,8 @@ Client.prototype.searchPlaces = function(callback) {
 		}
 	};
 
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
 	xhr.open('GET', that.ENDPOINT+"places/search.json?key=" + that.COCOAFISH_APPLICATION_KEY);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send();
 };
 
@@ -124,7 +116,7 @@ Client.prototype.createPlace = function(args, callback) {
 	xhr.onerror = function(r) {
 		var alertDialog = Titanium.UI.createAlertDialog({
 			title : '',
-			message : 'Unable to connect server. Please check network connection',
+			message : 'Please check network connection (are you logged in?)',
 			buttonNames : ['OK']
 		});
 		alertDialog.show();
@@ -153,9 +145,9 @@ Client.prototype.createPlace = function(args, callback) {
 		}
 	};
 
+	xhr.open('POST', that.ENDPOINT+"places/create.json?key=" + that.COCOAFISH_APPLICATION_KEY);
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-	xhr.open('POST', that.ENDPOINT+"places/create.json.json?key=" + that.COCOAFISH_APPLICATION_KEY);
+    xhr.setRequestHeader("Cookie", "_session_id="+Ti.App.Properties.getString('session_id'));
 	xhr.send(params);
 };
 exports.Client = Client;
